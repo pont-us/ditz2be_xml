ed924d48fd20705db72513422499fec9bdddf3dc
Pont Lurcock <pont@talvi.net>
2014-10-05 13:50:47 +0200

    Convert status and timestamp; fix Ditz YAML problems.
     - Map Ditz status string to nearest equivalent BE status.
    
     - Convert Ditz timestamp string format to BE format.
    
     - Preprocess Ditz YAML to fix a couple of deviations from
       the YAML specification which would otherwise confuse yaml.load.

--- ditz2be_xml.py
+++ ditz2be_xml.py
@@ -15,10 +15,20 @@ import logging
 import glob
 import os.path
 import io
+import re
+
 logging.basicConfig(format='%(levelname)s:%(funcName)s:%(message)s',
                     level=logging.DEBUG)
+
 DITZ_DIR = ".ditz/"
 
+# A mapping from Ditz status strings to Bugs Everywhere status strings.
+STATUS_MAP = \
+{":unstarted"   : "open",
+ ":paused"      : "open",
+ ":in_progress" : "open",
+ ":closed"      : "fixed"
+}
 
 def _xml_indent(elem, level=0):
     """Indent an XML element and its children.
@@ -91,14 +101,22 @@ class Issue(yaml.YAMLObject):
 
     @staticmethod
     def __format_time(in_time):
-        """
-        in_time is datetime.datetime
-        example: "2012-02-23T22:09:58Z"
+        """Convert a Ditz timestamp to a BE timestamp.
+        
+        Args:
+            in_time: timestamp string with format "2012-02-23T22:09:58Z"
+
+        Return:
+            timestamp string with format "Thu, 23 Feb 2012 22:09:58 +0000"
         """
         logging.debug("in_time = %s", in_time)
-        out = in_time.strftime("%a, %d %b %Y %H:%M:%S %z")
+        out = in_time.strftime("%a, %d %b %Y %H:%M:%S %z+0000")
         logging.debug("out = %s, out")
         return out
+
+    @staticmethod
+    def __convert_status(ditz_status):
+        return STATUS_MAP[ditz_status]
 
     def __add_subelement(self, iss_attr, trg_elem, convert=None):
         if getattr(self, iss_attr) is not None:
@@ -117,7 +135,8 @@ class Issue(yaml.YAMLObject):
         self.__add_subelement("creation_time", "created",
                               self.__format_time)
         self.__add_subelement("title", "summary")
-        self.__add_subelement("status", "status")
+        self.__add_subelement("status", "status",
+                              self.__convert_status)
         self.__add_subelement("reporter", "reporter")
         self.__add_subelement("reporter", "creator")
         # FIXME
@@ -171,6 +190,39 @@ class Component(yaml.YAMLObject):
     def __init__(self, name):
         self.name = name
 
+def fix_ditz_yaml(ditz_string):
+    """Correct problems with Ditz YAML.
+
+    Ditz doesn't put double quotes around titles starting with the
+    "`" character, and encodes non-ASCII characters in a non-standard
+    way. This function rewrites a Ditz quasi-YAML into a well-formed
+    YAML string which can be read by yaml.load without problems.
+
+    Args:
+        ditz_string: an issue record in "ditz-flavoured" YAML
+
+    Return:
+        the equivalent record in specification-compliant YAML 1.1
+    """
+
+    # Put " characters around titles beginning with `
+    result = re.sub("title: (`.*)\n", r'title: "\1"\n', ditz_string)
+
+    # Ditz represents 16-bit Unicode characters as three consecutive
+    # \x.. escapes representing the bytes of a UTF-8 encoding,
+    # rather than (as YAML specifies) a \u.... escape or the encoded
+    # UTF-8 character itself (UTF-8 is YAML's default encoding anyway
+    # so escapes are unnecessary). However, we can't just do a blanket
+    # decode on the whole string since it would also replace \n escapes
+    # required for multiple paragraphs in text block (and possibly other
+    # things I haven't thought of). So we selectively decode the \x
+    # escapes only.
+    matches = re.findall(r"((\\x..)+)", result)
+    for escape, _ in matches:
+        utf8 = escape.decode("string_escape").decode("utf-8")
+        result = result.replace(escape, utf8)
+
+    return result
 
 def main():
     out_xml = et.fromstring("""<be-xml>
@@ -182,7 +234,10 @@ def main():
     logging.debug("issue_files = %s", issue_files)
 
     for i_file in issue_files:
-        issue = yaml.load(io.open(i_file))
+        with io.FileIO(i_file) as issue_io:
+            issue_io = io.FileIO(i_file)
+            issue_str = issue_io.readall()
+        issue = yaml.load(fix_ditz_yaml(issue_str))
         out_xml.append(issue.to_XML())
 
     _xml_indent(out_xml)
